11d36ab5612a1b07e4c0cfe24d1f1b7fbc7f7c96
Kevin <42954357@fi.unju.edu.ar>
2023-09-08 14:56:43 -0300

    clase - 08/09

---
+++ clase_08_09.py
@@ -0,0 +1,57 @@
+import imageio.v2 as imageio
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def to_yiq(im_rgb):
+    new_yiq = np.zeros(im_rgb.shape)
+    new_yiq[:, :, 0] = np.clip(
+        0.229 * im_rgb[:, :, 0] + 0.587 * im_rgb[:, :, 1] + 0.114 * im_rgb[:, :, 2],
+        a_min=0,
+        a_max=1
+    )
+    new_yiq[:, :, 1] = np.clip(
+        0.595716 * im_rgb[:, :, 0] + -0.274453 * im_rgb[:, :, 1] + -0.321263 * im_rgb[:, :, 2],
+        a_min=-0.5957,
+        a_max=0.5957
+    )
+    new_yiq[:, :, 2] = np.clip(
+        0.211456 * im_rgb[:, :, 0] + -0.522591 * im_rgb[:, :, 1] + 0.311135 * im_rgb[:, :, 2],
+        a_min=-0.5226,
+        a_max=0.5226
+    )
+    return new_yiq
+
+
+def to_rgb(im_yiq):
+    new_rgb = np.zeros(im_yiq.shape)
+    new_rgb[:, :, 0] = im_yiq[:, :, 0] + 0.9663 * im_yiq[:, :, 1] + 0.6210 * im_yiq[:, :, 2]
+    new_rgb[:, :, 1] = im_yiq[:, :, 0] + -0.2721 * im_yiq[:, :, 1] + -0.6474 * im_yiq[:, :, 2]
+    new_rgb[:, :, 2] = im_yiq[:, :, 0] + -1.1070 * im_yiq[:, :, 1] + 1.7046 * im_yiq[:, :, 2]
+    new_rgb = (new_rgb * 255).astype(int)
+    return np.clip(new_rgb, 0, 255)
+
+
+im1 = imageio.imread('resource/image1.png')
+im1 = np.clip(im1 / 255., 0., 1.)
+im2 = imageio.imread('resource/image2.png')
+im2 = np.clip(im2 / 255., 0., 1.)
+
+ima = im1 + im2
+imb = ima.copy() / 2
+#ima = np.clip(ima / 255., 0., 1.)
+#imb = np.clip(imb / 255., 0., 1.)
+
+imc = to_yiq(im1)
+imd = to_yiq(im2)
+
+imyiq = np.zeros(imc.shape)
+
+imyiq[:, :, 0] = (imc[:, :, 0] + imd[:, :, 0]) / 2
+imyiq[:, :, 1] = ((imc[:, :, 0] * imc[:, :, 1]) + (imd[:, :, 0] * imd[:, :, 1])) / (imc[:, :, 0] + imd[:, :, 0])
+imyiq[:, :, 2] = ((imc[:, :, 0] * imc[:, :, 2]) + (imd[:, :, 0] * imd[:, :, 2])) / (imc[:, :, 0] + imd[:, :, 0])
+
+plt.imshow(imb)
+plt.show()
+plt.imshow(to_rgb(imyiq))
+plt.show()
